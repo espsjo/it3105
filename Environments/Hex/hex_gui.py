163f6dc21f7e7fa289f5143c5e26cc8556737eaa
espeasj <espeasj@stud.ntnu.no>
2023-01-27 00:55:03 +0100

    very simple ui for visualizing, needs improvement

--- Environments/Hex/hex_gui.py
+++ Environments/Hex/hex_gui.py
@@ -1,2 +1,35 @@
+from matplotlib import pyplot as plt
+from numpy import cos, sin, pi
+
+
 class HexGUI:
-    pass
+    def __init__(self, Hex):
+        self.Hex = Hex
+
+    def visualize(self, history, winner):
+        self.hist = history
+        plt.axes()
+        ax = plt.gca()
+        t1 = cos(pi/4)
+        t2 = sin(pi/4)
+        d = {0: 'white', 1: 'red', 2: 'blue'}
+        for board in self.hist:
+            for i, j in enumerate(board.flatten()):
+
+                cell = self.Hex.convert_node(i, isflattened=True)
+
+                circle = plt.Circle(
+                    (cell[0]*t1-cell[1]*t2, cell[0]*t2 + cell[1]*t1),
+                    0.3,
+                    linewidth=3,
+                    ec='black',
+                    fc=d[j],
+
+                )
+                ax.add_patch(circle)
+                ax.axis('off')
+                plt.axis('scaled')
+
+            plt.pause(0.25)
+        plt.title(f"The winner was {d[winner]} (player {winner}) ")
+        plt.show()
