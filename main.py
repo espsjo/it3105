aee33f3090a4b884c3334e034f1f3b9baf940fd8
espeasj <espeasj@stud.ntnu.no>
2023-01-27 00:54:23 +0100

    main method only for testing atm

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from Environments.Hex.hex import Hex
+from Environments.Hex.hex_gui import HexGUI
 from constants import Constants
+import numpy as np
 
 
 def main():
     h = Hex(Constants.BOARD_SIZE)
-    pass
+    hv = HexGUI(h)
+    while not h.winner:
+        move = int(np.random.choice(h.get_legal_moves()))
+        h.play_move(move)
+    hv.visualize(h.board_hist, h.winner)
 
 
 if __name__ == '__main__':
