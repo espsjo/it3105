0c9c24cf84828c2e3348a61d096054db9f0d0351
espeasj <espeasj@stud.ntnu.no>
2023-01-27 09:20:07 +0100

    updated gui to yield more information

--- Environments/Hex/hex_gui.py
+++ Environments/Hex/hex_gui.py
@@ -1,13 +1,17 @@
 from matplotlib import pyplot as plt
 from numpy import cos, sin, pi
+from .hex import Hex
 
 
 class HexGUI:
-    def __init__(self, Hex):
+    def __init__(self, Hex: Hex):
         self.Hex = Hex
+        self.hist = self.Hex.board_hist
+        self.winner = self.Hex.winner
+        self.winning_island = self.Hex.winning_island
+        self.visualize()
 
-    def visualize(self, history, winner):
-        self.hist = history
+    def visualize(self):
         plt.axes()
         ax = plt.gca()
         t1 = cos(pi/4)
@@ -31,5 +35,34 @@ class HexGUI:
                 plt.axis('scaled')
 
             plt.pause(0.25)
-        plt.title(f"The winner was {d[winner]} (player {winner}) ")
+        plt.title(f"The winner was {d[self.winner]} (player {self.winner}) ")
+
+        for i, j in enumerate(self.hist[-1].flatten()):
+
+            cell = self.Hex.convert_node(i, isflattened=True)
+
+            if not i in self.winning_island:
+
+                circle = plt.Circle(
+                    (cell[0]*t1-cell[1]*t2, cell[0]*t2 + cell[1]*t1),
+                    0.3,
+                    linewidth=3,
+                    ec='black',
+                    fc=d[j],
+
+                )
+            else:
+                circle = plt.Circle(
+                    (cell[0]*t1-cell[1]*t2, cell[0]*t2 + cell[1]*t1),
+                    0.3,
+                    linewidth=3,
+                    ec='darkorange',
+                    fc=d[j],
+
+                )
+            ax.add_patch(circle)
+            ax.axis('off')
+            plt.axis('scaled')
+
+        plt.pause(0.001)
         plt.show()
