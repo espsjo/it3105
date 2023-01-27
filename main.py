c68bc0364aa67d44b0ce6c0da90401dcc1373987
espeasj <espeasj@stud.ntnu.no>
2023-01-27 09:19:49 +0100

    main clean

--- main.py
+++ main.py
@@ -6,11 +6,10 @@ import numpy as np
 
 def main():
     h = Hex(Constants.BOARD_SIZE)
-    hv = HexGUI(h)
     while not h.winner:
         move = int(np.random.choice(h.get_legal_moves()))
         h.play_move(move)
-    hv.visualize(h.board_hist, h.winner)
+    hv = HexGUI(h)
 
 
 if __name__ == '__main__':
