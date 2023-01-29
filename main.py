2492e6579be9bdf606cc70af7cb831399b154082
espeasj <espeasj@stud.ntnu.no>
2023-01-29 23:12:17 +0100

    changed main a bit to support new structure

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 from Environments.Hex.hex import Hex
-from Environments.Hex.hex_gui import HexGUI
-from constants import Constants
+from constants import hex_config, config
 import numpy as np
 
 
 def main():
-    h = Hex(Constants.BOARD_SIZE)
+    h = Hex(hex_config, visualize=True)
     while not h.winner:
         move = int(np.random.choice(h.get_legal_moves()))
         h.play_move(move)
-    hv = HexGUI(h, ANIMATION_SPEED=Constants.ANIMATION_SPEED)
 
 
 if __name__ == '__main__':
