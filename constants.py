37358587694d85b5cd4e37ddf97000dc351e0799
espeasj <espeasj@stud.ntnu.no>
2023-01-30 10:52:15 +0100

    constants for NIM

--- constants.py
+++ constants.py
@@ -5,7 +5,15 @@ config = {
 
 hex_config = {
 
-    'BOARD_SIZE': 4,
+    'BOARD_SIZE': 5,
     'ANIMATION_SPEED': 0.25
+
+}
+
+nim_config = {
+
+    'STONES': 10,
+    'MIN_STONES': 1,
+    'MAX_STONES': 2
 
 }
